dc28d69ae1fa9cf18164532f92fdb3dd6fc85928
Nora Abi Akar <nora.abiakar@gmail.com>
2022-10-18 17:34:43 +0200

    Update keyvault.ps1

--- .github/workflows/keyvault.ps1
+++ .github/workflows/keyvault.ps1
@@ -3,9 +3,7 @@ param(
 )
 $map = @{}
 for ($i=0; $i -lt $array.length; $i++){ 
-  $res = az keyvault secret show --name "secret1" --vault-name "nabki-keyvault" --query value -o tsv 
+  $res = az keyvault secret show --name $array[$i] --vault-name "nabki-keyvault" --query value -o tsv 
   $map.Add($array[$i], $res)
 }
 return $map
-
-#.\test1.ps1 @('secret1', 'secret2', 'secret3')
