0f10d690e23573e3e43f9f0277af0e1fa2190dc1
Nora Abi Akar <noraabiakar@microsoft.com>
2022-10-19 11:00:29 +0200

    small fix

--- .github/scripts/keyvault.ps1
+++ .github/scripts/keyvault.ps1
@@ -3,8 +3,8 @@ param(
 )
 
 $array | ForEach-Object -Parallel {
-  $key = $array[$i]
+  $key = $_
   $val = az keyvault secret show --name $key --vault-name "nabki-keyvault" --query value -o tsv 
   "$key = $val" >> $env:GITHUB_ENV
   Write-Host("::add-mask::$val")
-}
+} -ThrottleLimit 10
