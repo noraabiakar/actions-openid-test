87e50d9936cee62a966385d90afda78595b61715
Nora Abi Akar <noraabiakar@microsoft.com>
2022-10-19 10:53:14 +0200

    small fixes

--- .github/scripts/keyvault.ps1
+++ .github/scripts/keyvault.ps1
@@ -1,9 +1,9 @@
 param(
   [array]$array
 )
-$map = @{}
 for ($i=0; $i -lt $array.length; $i++){ 
-  $res = az keyvault secret show --name $array[$i] --vault-name "nabki-keyvault" --query value -o tsv 
-  "$array[$i] = $res" >> $env:GITHUB_ENV
-  Write-Host("::add::mask::$res")
+  $key = $array[$i]
+  $val = az keyvault secret show --name $key --vault-name "nabki-keyvault" --query value -o tsv 
+  "$key = $val" >> $env:GITHUB_ENV
+  Write-Host("::add-mask::$val")
 }
