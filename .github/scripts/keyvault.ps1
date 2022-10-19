f4ccae9b972397f948c793df9da0bdd464ea5ae9
Nora Abi Akar <noraabiakar@microsoft.com>
2022-10-19 10:47:27 +0200

    obfuscate secret values

--- .github/scripts/keyvault.ps1
+++ .github/scripts/keyvault.ps1
@@ -4,6 +4,6 @@ param(
 $map = @{}
 for ($i=0; $i -lt $array.length; $i++){ 
   $res = az keyvault secret show --name $array[$i] --vault-name "nabki-keyvault" --query value -o tsv 
-  $map.Add($array[$i], $res)
+  "$array[$i] = $res" >> $env:GITHUB_ENV
+  Write-Host("::add::mask::$res")
 }
-return $map
